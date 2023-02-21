b3e51cf94602579fff6b26067442afac341e6e83
majh <majh@cunxin.com>
2023-02-21 23:35:00 +0800

    uodate

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,11 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://chat-tan.shirtiny.cn" target="self">
+          <img src="/logo.svg" className="logo" alt="logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Chat - Tan， 茶与青春范</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
