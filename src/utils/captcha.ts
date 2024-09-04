d816ddda604713e49ee33b020b0e6e5d78dcde02
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2024-09-04 11:56:15 +0000

    gen token

--- src/utils/captcha.ts
+++ src/utils/captcha.ts
@@ -3,7 +3,7 @@ import logger from "@/utils/logger";
 const genToken = (action = "login") => {
     const grecaptcha = (window as any).grecaptcha;
     
-    return new Promsie(r => {
+    return new Promise(r => {
         grecaptcha.ready(async () => {
             const token = await grecaptcha.execute(
               "6Lf8ohclAAAAAInG1aKYnPBL4129L8vP6ENZtNo4",
