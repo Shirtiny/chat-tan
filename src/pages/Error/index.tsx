30605bf5fedf94aff57b1b67f9269e9c97a6b1c6
Shirtiny <49592759+Shirtiny@users.noreply.github.com>
2024-09-04 10:57:27 +0000

    update

--- src/pages/Error/index.tsx
+++ src/pages/Error/index.tsx
@@ -1,6 +1,7 @@
 import { useRouteError } from "react-router-dom";
 const ErrorPage = () => {
   const error = useRouteError() as any;
+  console.error("error", error);
   return <div className="page page-error">Oops! {error.statusText || error.message}</div>;
 };
 
