b18545e0b2e6073ed907fb0d88685907b9e7e1dc
majunhu <majh@cunxin.com>
2024-09-30 14:43:41 +0800

    add Input

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,69 @@
+import {
+  useCallback,
+  type ChangeEvent,
+  type FC,
+  type InputHTMLAttributes,
+} from "react";
+import type { ICommonProps } from "@/types";
+import component from "@/hoc/component";
+import { cls } from "@shirtiny/utils/lib/style";
+import css from "./index.module.scss";
+
+interface IProps extends InputHTMLAttributes<HTMLInputElement>, ICommonProps {
+  name?: string;
+  status?: "normal" | "error";
+  /**
+   * @deprecated input的最小高度 建议使用className控制
+   */
+  height?: number;
+  maxLength?: number;
+
+  onChange?(e: ChangeEvent<HTMLInputElement>): void;
+}
+
+const Input: FC<IProps> = (
+  {
+    className,
+    style,
+    name,
+    status = "normal",
+    autoComplete = "off",
+    maxLength = 99,
+    disabled,
+    readOnly,
+    height,
+    onChange,
+    ...rest
+  },
+  ref,
+) => {
+  const handleChange = useCallback(() => {
+    (e: ChangeEvent<HTMLInputElement>) => {
+      onChange && onChange(e);
+    };
+  }, []);
+
+  const statusCls = css[`status-${status}`];
+
+  return (
+    <input
+      ref={ref}
+      className={cls(
+        css.input,
+        className,
+        statusCls,
+        (readOnly || disabled) && css.disabled,
+      )}
+      style={style}
+      name={name}
+      disabled={disabled}
+      readOnly={readOnly}
+      autoComplete={autoComplete}
+      maxLength={maxLength}
+      onChange={handleChange}
+      {...rest}
+    />
+  );
+};
+
+export default component<IProps>(Input, { useForwardRef: true });
